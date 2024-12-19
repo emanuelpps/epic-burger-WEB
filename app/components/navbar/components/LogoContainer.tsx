c7f08b065331e8f8c92f449610588f42d1285600
emanuelpps <emanuelpages.ps@gmail.com>
2024-12-19 20:15:56 -0300

    fix: correction in navbar logo position

--- app/components/navbar/components/LogoContainer.tsx
+++ app/components/navbar/components/LogoContainer.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import NavBarLogoImage from "@/public/assets/images/navbar-logo.svg";
 import Image from "next/image";
 
-
 const NavBarLogo = () => {
   return (
-    <div id="navbar-logo" className="w-full">
-    <Image
-      src={NavBarLogoImage}
-      width={80}
-      height={80}
-      alt="Epic Burger Logo"
-    />
-  </div>
-  )
-}
+    <div
+      id="navbar-logo"
+      className="flex items-center justify-start w-full pl-20"
+    >
+      <Image
+        src={NavBarLogoImage}
+        width={80}
+        height={80}
+        alt="Epic Burger Logo"
+      />
+    </div>
+  );
+};
 
-export default NavBarLogo
+export default NavBarLogo;
