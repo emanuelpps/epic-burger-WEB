f980ba70be31cbe29636bb542223167d5463147f
emanuelpps <emanuelpages.ps@gmail.com>
2025-01-22 18:35:03 -0300

    feat: added dot in slides indicator

--- app/menu/burgers/components/Slide.tsx
+++ app/menu/burgers/components/Slide.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
 
 export const Slide = () => {
+  const [slide, setSlide] = useState(0);
+  const slides = [
+    "https://res.cloudinary.com/dehyuw4zu/image/upload/f_auto,q_auto/1-min_fhwgaa",
+    "https://res.cloudinary.com/dehyuw4zu/image/upload/f_auto,q_auto/2-min_sxeq6t",
+    "https://res.cloudinary.com/dehyuw4zu/image/upload/f_auto,q_auto/1-min_fhwgaa",
+  ];
   return (
-    <div>
+    <div className="mt-10">
       <Image
-        src="https://res.cloudinary.com/dehyuw4zu/image/upload/f_auto,q_auto/1-min_fhwgaa"
+        src={slides[slide]}
         alt="burger promo barbacoa"
         width={900}
         height={600}
         className="object-cover rounded-3xl"
       />
+      <div id="dot-container" className="flex relative">
+        <div className="absolute z-40 bottom-5 left-1/2 transform -translate-x-1/2 rounded-full flex gap-2">
+          <div className="bg-white w-3 h-3 rounded-full"></div>
+          <div className="bg-slate-400 w-3 h-3 rounded-full"></div>
+          <div className="bg-slate-400 w-3 h-3 rounded-full"></div>
+        </div>
+      </div>
     </div>
   );
 };
