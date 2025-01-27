b6112da51a26257a4d2db9b9e3e5fb13911bfa5a
emanuelpps <emanuelpages.ps@gmail.com>
2025-01-27 11:42:22 -0300

    feat: added static data to render product card

--- app/menu/burgers/components/ProductContainer.tsx
+++ app/menu/burgers/components/ProductContainer.tsx
@@ -1,10 +1,19 @@
 import { ProductCard } from "@/app/components/productCard/ProductCard";
 import React from "react";
+import Burgers from "@/public/assets/data/burgers.json";
 
 export const ProductContainer = () => {
   return (
-    <div>
-      <ProductCard />
+    <div className="w-[80%] flex justify-center items-center flex-wrap gap-20">
+      {Burgers.map((burger, index) => (
+        <ProductCard
+          key={index}
+          title={burger.title}
+          price={burger.price}
+          rating={burger.rating ?? 0}
+          image={burger.image}
+        />
+      ))}
     </div>
   );
 };
