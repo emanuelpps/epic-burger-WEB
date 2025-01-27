04c047cb4fa2186918f3dec0da0461eb847b38e2
emanuelpps <emanuelpages.ps@gmail.com>
2025-01-27 11:30:23 -0300

    style: added styles and icons to product card

--- app/components/productCard/ProductCard.tsx
+++ app/components/productCard/ProductCard.tsx
@@ -1,18 +1,59 @@
+import Image from "next/image";
 import React from "react";
+import { HiOutlinePlusSm } from "react-icons/hi";
+import { FcLikePlaceholder } from "react-icons/fc";
+import { FcLike } from "react-icons/fc";
 
-export const ProductCard = () => {
+interface ProductCardProps {
+  title: string;
+  price: number;
+  discount?: number;
+  rating: number;
+  image: string;
+  descrption?: string;
+}
+
+export const ProductCard: React.FC<ProductCardProps> = ({
+  title,
+  price,
+  discount,
+  rating,
+  image,
+  descrption,
+}) => {
   return (
     <div id="product-card-container">
       <div
         id="product-card"
-        className="flex flex-col h-60 bg-white rounded-lg shadow-lg w-48"
+        className="flex flex-col h-60 bg-white rounded-lg shadow-lg w-48 justify-center"
       >
-        <div id="product-header" className="">
-          <div id="discount-section">d</div>
-          <div id="like-section"></div>
-          <div id="product-image"></div>
-          <div id="product-rating"></div>
-          <div id="product-detail"></div>
+        <div id="product-header" className="flex justify-between w-[90%]">
+          <div id="discount-section">{discount ? discount : null}</div>
+          <div id="like-section">
+            <FcLikePlaceholder className="h-5 w-5" />
+          </div>
+        </div>
+        <div
+          id="product-image"
+          className="flex justify-center items-center w-full"
+        >
+          <Image src={image} alt="Burger image" width={200} height={150} />
+        </div>
+        <div id="product-rating">{rating}</div>
+        {
+          //hacer un map y setear las estrellas por debajo de .5 es el numero anterior y si es supero el numero siguiente
+        }
+        <div id="product-detail" className="flex justify-between w-[90%]">
+          <div id="price-title">
+            <div id="product-title">{title}</div>
+            <div id="product-price">{price}</div>
+          </div>
+          <div
+            id="add-button"
+            className="flex justify-center items-center bg-[#F8B602] rounded-s-sm"
+          >
+            <HiOutlinePlusSm className="text-white h-6 w-6 p-1" />
+          </div>
         </div>
       </div>
     </div>
