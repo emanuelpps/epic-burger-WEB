fb3de78154179e28e2f98e7930d3694ffe953093
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-06 18:53:06 -0300

    fix: productcard fix

--- app/components/productCard/ProductCard.tsx
+++ app/components/productCard/ProductCard.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import React from "react";
 import { HiOutlinePlusSm } from "react-icons/hi";
 import { FcLikePlaceholder } from "react-icons/fc";
-import { FcLike } from "react-icons/fc";
 
 interface ProductCardProps {
   title: string;
