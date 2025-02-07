d3027d890647b2d2348052d89e62cf04913edf56
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-07 09:42:39 -0300

    feat: added logic dropdown render in profile avatar

--- app/components/navbar/components/Avatar.tsx
+++ app/components/navbar/components/Avatar.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { RxAvatar } from "react-icons/rx";
+import ProfileDropdown from "./ProfileDropdown";
 
 export default function Avatar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-center items-center w-[10%]">
+    <div
+      className="relative flex justify-center items-center w-[10%] cursor-pointer"
+      onClick={() => setIsOpen(!isOpen)}
+    >
       <RxAvatar className="text-[#F23F39] text-[1.5rem]" />
+      {isOpen && <ProfileDropdown />}
     </div>
   );
 }
