bdc49c0f92acc2e72be807841120f05b5e1dab95
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-07 09:49:54 -0300

    feat: added dropdown menu in profile avatar

--- app/components/navbar/components/ProfileDropdown.tsx
+++ app/components/navbar/components/ProfileDropdown.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 const ProfileDropdown = () => {
-  return <div>ProfileDropdown</div>;
+  return (
+    <div className="absolute bg-[#f0eeeb] w-40 h-40 top-6 right-3">
+      <div>
+        <Link href="/profile" className="block p-2">
+          Mi Perfil
+        </Link>
+        <Link href="/favoritos" className="block p-2">
+          Favoritos
+        </Link>
+        <Link href="/mis-pedidos" className="block p-2">
+          Mis Pedidos
+        </Link>
+        <Link href="/logout" className="block p-2">
+          Cerrar Sesión
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileDropdown;
