8897c7025f11549412767ef8672e2f5b4dd8471a
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-10 11:20:41 -0300

    feat: added new section

--- app/menu/bebidas/page.tsx
+++ app/menu/bebidas/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { ProductContainer } from "../burgers/components/ProductContainer";
+import { PrimaryTitle } from "@/app/components/titles/PrimaryTitle";
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full flex justify-center items-center h-full flex-col bg-[#FFFBF2]">
+      <div className="w-[100%] flex justify-center items-center">
+        <div className="w-[80%] mt-10">
+          <PrimaryTitle text="Bebidas" />
+        </div>
+      </div>
+      <ProductContainer />
+    </div>
+  );
 };
 
 export default Page;
