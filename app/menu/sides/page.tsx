11c7de27bccdc016a1edac847c55353ace11b35b
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-10 11:14:51 -0300

    feat: added sides structure page

--- app/menu/sides/page.tsx
+++ app/menu/sides/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { ProductContainer } from "../burgers/components/ProductContainer";
+import { PrimaryTitle } from "@/app/components/titles/PrimaryTitle";
 
 const Page = () => {
-  return <div>P</div>;
+  return (
+    <div className="w-full flex justify-center items-center h-full flex-col bg-[#FFFBF2]">
+      <div className="w-[100%] flex justify-center items-center">
+        <div className="w-[80%] mt-10">
+          <PrimaryTitle text="Sides" />
+        </div>
+      </div>
+      <ProductContainer />
+    </div>
+  );
 };
 
 export default Page;
