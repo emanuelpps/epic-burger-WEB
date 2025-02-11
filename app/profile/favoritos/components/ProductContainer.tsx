13b35b7fc6829a5fd2819cc198abc29c92509bd7
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-11 16:09:20 -0300

    fix: style correction to title in product favorite list

--- app/profile/favoritos/components/ProductContainer.tsx
+++ app/profile/favoritos/components/ProductContainer.tsx
@@ -1,3 +1,4 @@
+import { PrimaryTitle } from "@/app/components/titles/PrimaryTitle";
 import React from "react";
 
 const ProductContainer = () => {
@@ -5,7 +6,7 @@ const ProductContainer = () => {
     <div className="flex w-[100%] justify-center items-center mt-20">
       <div className="bg-[#F23F39] w-[80%] h-[500px] flex flex-col p-10 rounded-lg">
         <div className="flex">
-          <h2>Mis Favoritos</h2>
+          <PrimaryTitle text="Productos Favoritos" type="tertiary" />
         </div>
         <div className="bg-slate-100 h-full rounded-xl">gfdfs</div>
       </div>
