f7ae3ae761d7ae3a3a9cde545b915445e16fb365
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-11 15:56:25 -0300

    feat: added favorite product page to profile

--- app/profile/favoritos/components/ProductContainer.tsx
+++ app/profile/favoritos/components/ProductContainer.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const ProductContainer = () => {
-  return <div>ProductContainer</div>;
+  return (
+    <div className="flex w-[100%] justify-center items-center mt-20">
+      <div className="bg-[#F23F39] w-[80%] h-[500px] flex flex-col p-10 rounded-lg">
+        <div className="flex">
+          <h2>Mis Favoritos</h2>
+        </div>
+        <div className="bg-slate-100 h-full rounded-xl">gfdfs</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductContainer;
--- app/profile/favoritos/page.tsx
+++ app/profile/favoritos/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductContainer from "./components/ProductContainer";
 
 const Pages = () => {
-  return <div>pages</div>;
+  return (
+    <div className="w-[100%] h-[100%] flex justify-center items-center">
+      <ProductContainer />
+    </div>
+  );
 };
 
 export default Pages;
