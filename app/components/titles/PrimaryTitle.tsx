01d4b22353dcacb69642ac65f4882a23f1e51a8d
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-11 16:01:06 -0300

    fix: title component refactor

--- app/components/titles/PrimaryTitle.tsx
+++ app/components/titles/PrimaryTitle.tsx
@@ -1,7 +1,27 @@
 interface TitleProps {
   text: string;
+  type: "primary" | "secondary" | "tertiary";
 }
 
-export const PrimaryTitle: React.FC<TitleProps> = ({ text }) => {
-  return <h1 className="text-[3rem] font-[700] text-[#F23F39]">{text}</h1>;
+const titleStyles = {
+  primary: {
+    fontSize: "3rem",
+    fontWeight: 700,
+    color: "#F23F39",
+  },
+  secondary: {
+    fontSize: "2.5rem",
+    fontWeight: 600,
+    color: "#F23F39",
+  },
+  tertiary: {
+    fontSize: "2rem",
+    fontWeight: 500,
+    color: "#FFFBF2",
+  },
+};
+
+export const PrimaryTitle: React.FC<TitleProps> = ({ text, type }) => {
+  const style = titleStyles[type];
+  return <h1 style={style}>{text}</h1>;
 };
