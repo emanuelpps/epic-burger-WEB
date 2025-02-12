c86091d7a4d54337cd236efa558c51d2f79e97cf
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-12 17:04:28 -0300

    feat: added basic structure for orders by user profile

--- app/profile/pedidos/components/Order.tsx
+++ app/profile/pedidos/components/Order.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+interface OrderProps {
+  order: {
+    id: number;
+    order: string;
+    date: string;
+    total: number;
+    address: string;
+  };
+}
 
-const Order = () => {
-  return <div>Order</div>;
+const Order: React.FC<OrderProps> = ({
+  order: { id, order, date, total, address },
+}) => {
+  return (
+    <div>
+      <h2>Order ID: {id}</h2>
+      <h2>Order: {order}</h2>
+      <h2>Date: {date}</h2>
+      <h2>Total: {total}</h2>
+      <h2>Address: {address}</h2>
+    </div>
+  );
 };
 
 export default Order;
--- app/profile/pedidos/components/OrdersContainer.tsx
+++ app/profile/pedidos/components/OrdersContainer.tsx
@@ -1,10 +1,45 @@
+"use client";
+import { useState } from "react";
 import BoxContainer from "../../components/BoxContainer";
+import Order from "./Order";
 
 const OrdersContainer = () => {
+  const [ordersData] = useState([
+    {
+      id: 1,
+      order: "Orden 1",
+      date: "01/01/2021",
+      total: 100,
+      address: "Calle 1",
+    },
+    {
+      id: 2,
+      order: "Orden 2",
+      date: "02/02/2021",
+      total: 200,
+      address: "Calle 2",
+    },
+    {
+      id: 3,
+      order: "Orden 3",
+      date: "03/03/2021",
+      total: 300,
+      address: "Calle 3",
+    },
+    {
+      id: 4,
+      order: "Orden 4",
+      date: "04/04/2021",
+      total: 400,
+      address: "Calle 4",
+    },
+  ]);
   return (
     <>
       <BoxContainer text="Mis Pedidos" type="tertiary">
-        <div>hola</div>
+        {ordersData.map((order) => (
+          <Order key={order.id} order={order} />
+        ))}
       </BoxContainer>
     </>
   );
