6249d71f999a42239f8ce31dc6754ec23038ab2c
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-13 16:03:23 -0300

    style: added basic styles to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import CartContainer from "./components/CartContainer";
+import { PrimaryTitle } from "../components/titles/PrimaryTitle";
 
 const Page = () => {
-    return <div>Page</div>;
+  return (
+    <div className="w-[90%] mx-auto min-h-[50vh]">
+      <div className="mt-8 mb-4">
+        <PrimaryTitle text="Mi Carrito" type="primary" />
+      </div>
+      <CartContainer />
+    </div>
+  );
 };
 
 export default Page;
